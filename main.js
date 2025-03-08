09d7a53ce4105fbf298ddbdd965ecf0279d9a89c
DeNarr <rjsrz9@gmail.com>
2025-03-08 11:51:41 -0600

    Finished project

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let name = "Ryan"
-console.log(`moo!, ${name}`)
+import { moo } from "./moo.js";
+import { say } from "cowsay"
+
+const name = "Ryan";
+const message = moo(name);
+console.log(say({ text: message }));
